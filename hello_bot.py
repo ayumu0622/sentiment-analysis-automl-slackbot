8e27d507345751b0a32f6842aa288f16561ffdee
Ayumu <ayumu.justin.ueda@gmail.com>
2022-08-07 19:46:15 -0700

    done

--- hello_bot.py
+++ hello_bot.py
@@ -19,18 +19,7 @@ SLACK_APP_TOKEN = os.environ["socketoken"]
 
 app = App(token = SLACK_BOT_TOKEN)
 
-#main_data = get_value()
-main_data = {'Ayumu': [['motivation', ':wave:', ':wave:', ':wave:', ':wave:', 'hello', 'I am Ayumu', 'hello-socket-mode', 
-'/ hello-socket-mode', 'hello-socket-mode', '/ hello-socket-mode', '/ hello-socket-mode', 'hello', 'motivation', 'ji', 'hello', 'okay sure', 'ok sure', 
-'that is a good idea', 'this is so boring job', 'this task is not difficult to do I guess', "I don't like this job."], [0.699999988079071, 0.20000000298023224, 
-0.20000000298023224, 0.20000000298023224, 0.20000000298023224, 0.20000000298023224, 0.10000000149011612, 0.0, 0.0, 0.0, 0.0, 0.0, 0.20000000298023224, 0.699999988079071, 
-0.10000000149011612, 0.20000000298023224, 0.30000001192092896, 0.5, 0.8999999761581421, -0.8999999761581421, 0.10000000149011612, -0.8999999761581421]], 
-'Emma': [['I do not want to go to that meeting because it is always boring', 'I am Emma', 'yea that was hard'], [-0.800000011920929, 0.10000000149011612, -0.30000001192092896]], 
-'Bob': [['Tomorrow we have a meeting', 'I am Bob', 'It is so difficult to finish', 'I had a meeting from 6pm', 'Hello'],
- [0.0, 0.10000000149011612, -0.800000011920929, 0.0, 0.20000000298023224]], 
- 'Kate': [[':star-struck:', ':smile:', 'Me tooo', 'I am Kate', 'you are fired', 'I hope I can finish this by tomorrow'], [0.0, 0.6000000238418579,
-  0.699999988079071, 0.10000000149011612, -0.699999988079071, 0.20000000298023224]]}
-
+main_data = get_value()
 
 @app.message(re.compile("(hi|hello|hey)",re.IGNORECASE))
 def say_hello(message,say):
@@ -68,7 +57,7 @@ def answer4(message,say):
   if message["text"] in list(main_data.keys()):
     lista = main_data[message["text"]]
     for sentence, score in zip(lista[0],lista[1]):
-      say(f"{sentence}:{score}")
+      say(f"{sentence} : {score}")
   else:
     say("Try again")
 
